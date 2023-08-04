8adf78df9efb54c1c6f4bd4479c20945fd56bf81
vp-gsorin <georgio.sorin@vecteurplus.com>
2023-08-04 14:51:06 +0200

    mongoLinkconnect

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,9 +1,18 @@
 import * as mongoose from 'mongoose';
+import process from 'process';
+
+function mongodbLink() {
+  if(process.env.MONGO) {
+    return process.env.MONGO
+  } else {
+    return 'mongodb://localhost/nest'
+  }
+}
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+      mongoose.connect(mongodbLink()),
   },
 ];
